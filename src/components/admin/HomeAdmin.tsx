5e3341ca7c369a16dd7ced8643fabb8b2da536c8
Leonor <leonor.huanachin@gmail.com>
2023-07-19 22:00:59 -0500

    cambio Leo

--- src/components/admin/HomeAdmin.tsx
+++ src/components/admin/HomeAdmin.tsx
@@ -6,14 +6,23 @@ function HomeAdmin(){
         <>
         <div className='contenedor'>
             <div className='contenedor1'>
-                <div className='color1'>
-                </div>
-                <div className='color2'>
-                </div>
-
-            </div>
-            <div className='contenedor2'>
-
+                <header className='color1'>
+                    <img className='logo' src="src/assets/Logo.png"/>
+                    <h1 className='textoAdmin'>Admin</h1>
+                    <div className='usuario'>
+                        <img className='iconCustomer' src="src/assets/Customer.png" />
+                        <p>Federico</p>
+                    </div>
+                </header>
+                <main className='color2'>
+                    <button className='buttonHome'>Trabajadores</button>
+                    <button className='buttonHome'>Desayuno</button>
+                    <button className='buttonHome'>Almuerzo y Cena</button>
+                    <button className='buttonHome'>Pedidos</button>
+                </main>
+                <footer className='color3'>
+                <img className='iconLogout' src="src/assets/Logout.png" />
+                </footer>
             </div>
         </div>
     </>
