8bf0f120e55d289fe0691a8e55b736978ab372c5
Diana Clabel <dianaclabel08@gmail.com>
2023-07-21 23:50:02 -0500

    Se realiza funcionalidad de login

--- src/components/login/LoginPage.tsx
+++ src/components/login/LoginPage.tsx
@@ -1,7 +1,47 @@
-import burgerMobile from "../../../assets/burgerMobile.png";
-import logo from "../../../assets/logo.png";
+import { FormEventHandler } from "react";
+import burgerMobile from "../../assets/burgerMobile.png";
+import logo from "../../assets/logo.png";
+import { API_URL } from "../../constants";
 
 export const LoginPage = () => {
+  const login: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    console.log("login", event);
+
+    const formData = new FormData(event.target as HTMLFormElement);
+
+    const payload = {
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+
+    fetch(API_URL + "/login", {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => {
+        if (response.ok) {
+          alert("login exitoso");
+
+          response.json().then((data) => {
+            console.log(data);
+
+            /** @todo: Guardar el accessToken en localStorage y navegar al dashboard */
+          });
+        } else {
+          alert("Correo o contraseña incorrectos");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("No se pudo establecer una conexion");
+      });
+  };
+
   return (
     <div className="min-h-screen bg-orange-50">
       <div className="bg-orange-400 mb-16 pt-10">
@@ -13,7 +53,7 @@ export const LoginPage = () => {
         />
       </div>
       <div className="px-12">
-        <form>
+        <form onSubmit={login}>
           <div className="mb-4">
             <label className="block font-bold pb-2">Correo</label>
             <input
