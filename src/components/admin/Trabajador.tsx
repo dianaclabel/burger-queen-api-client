0499878a11a49f030ccc3e738accebd6baf89689
Leonor <leonor.huanachin@gmail.com>
2023-08-01 10:13:06 -0500

    cambio primer fetch

--- src/components/admin/Trabajador.tsx
+++ src/components/admin/Trabajador.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 export type TrabajadorProps = {
   email: string,
   pasword: string,
@@ -5,7 +7,32 @@ export type TrabajadorProps = {
   id: string,
 }
 
-export const Trabajador = (trabajador: TrabajadorProps) => {
+export const Trabajador = (props: TrabajadorProps) => {
+  const [trabajador, setTrabajador] = useState<TrabajadorProps | null>(null);
+
+  useEffect(() => {
+    const fetchTrabajadorData = async () => {
+      try{
+        const response = await fetch("http://localhost:8080/users",{
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdyYWNlLmhvcHBlckBzeXN0ZXJzLnh5eiIsImlhdCI6MTY5MDg2MTA4NiwiZXhwIjoxNjkwODY0Njg2LCJzdWIiOiIyIn0.Csq3MWaNdjsVk6oSma0OZlV-RsbHJjSH6GQ0w9fFEJM'
+          }
+        });
+        const data = await response.json();
+        setTrabajador(data);
+      } catch (error) {
+        console.error('Error al obtener los datos del trabajador:', error);
+      }
+    };
+    fetchTrabajadorData();
+  }, [props.id]);
+
+  if(!trabajador){
+    return <p>Cargando...</p>;
+  }
+
   return (
     <>
         <section className='boxTrabajador'>
@@ -14,8 +41,8 @@ export const Trabajador = (trabajador: TrabajadorProps) => {
                     <p>Role: {trabajador.role}</p>
             </div>
             <div className="containerIconsTrab">
-                    <img className="iconEdit" src="src/assets/Edit.png" />
-                    <img className="iconDelete" src="src/assets/Delete.png" />
+                    <img className="iconEdit" src="src/assets/Edit.png" alt="Editar" />
+                    <img className="iconDelete" src="src/assets/Delete.png" alt="Eliminar" />
             </div>
         </section>
     </>
