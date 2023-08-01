ac92e3f642763d6ed52c9a3da675d9fc344353f6
Leonor <leonor.huanachin@gmail.com>
2023-08-01 13:53:49 -0500

    cambio admin

---
+++ src/components/admin/compBodyAdmin/HeaderAdmin.tsx
@@ -0,0 +1,21 @@
+
+type HeaderProps = {
+    role: string;
+    userName: string;
+};
+
+export const HeaderAdmin = ({role, userName}: HeaderProps) => {
+  return (
+    <>
+        <header>
+            <img className='logo' src="../src/assets/Logo.png"/>
+                <h1 className='textoAdmin'>{role}</h1>
+            <div className='usuario'>
+                <img className='iconCustomer' src="../src/assets/iconsAdmin/Customer.svg" />
+                <p>{userName}</p>
+            </div>         
+        </header>
+    </>
+   
+  )
+}
