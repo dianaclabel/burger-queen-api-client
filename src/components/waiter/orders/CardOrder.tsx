758859c113161118d00f8d17be368f72daf03dce
Diana Clabel <dianaclabel08@gmail.com>
2023-08-08 16:46:40 -0500

    Se realiza estructura de la tarejta de pedidos

---
+++ src/components/waiter/orders/CardOrder.tsx
@@ -0,0 +1,11 @@
+const CardOrder = () => {
+  return (
+    <div>
+      <div>
+        <img src="/" alt="/" />
+      </div>
+      <p>Café americano</p>
+      <p>2</p>
+    </div>
+  );
+};
--- src/components/waiter/orders/OrdersPage.tsx
+++ src/components/waiter/orders/OrdersPage.tsx
@@ -1,3 +1,42 @@
 export const OrdersPage = () => {
-  return <main>Orders page</main>;
+  return (
+    <div>
+      <div>
+        <ul>
+          <li>
+            <p>N° de Pedido:</p>
+            <p>2</p>
+          </li>
+          <li>
+            <p>Cliente:</p>
+            <p>Pedrito Bravo</p>
+          </li>
+          <li>
+            <p>Hora:</p>
+            <p>7:40 a. m.</p>
+          </li>
+          <li>
+            <img src="/" alt="waiter" />
+            <p>Luis Garcia</p>
+          </li>
+        </ul>
+        <div>
+          <div>
+            <p>Preparado</p>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div>
+        <div>
+          <div>
+            <p>Descripción</p>
+            <p>Cantidad</p>
+          </div>
+          <div>//Colocar las tarejtas de pedidos</div>
+          <button>Entregar</button>
+        </div>
+      </div>
+    </div>
+  );
 };
